2c68719b73edbb7a12c915ed9a1da24bb01f1e08
Yuki-At <yuki.yuki.yoh@gmail.com>
2021-02-21 19:44:24 +0900

    メニューの表示は行けた.

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,10 +27,10 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
     }
 
     HWND hwnd = CreateWindowEx(
-        WS_EX_TOOLWINDOW | WS_EX_LAYERED,
+        WS_EX_TOOLWINDOW | WS_EX_LAYERED | WS_EX_TOPMOST,
         WindowClassName, TEXT("Battery Manager"),
         WS_POPUP,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 320, 240,
         nullptr, nullptr,
         hInstance, nullptr
     );
@@ -39,7 +39,11 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
         return -1;
     }
 
+    SetLayeredWindowAttributes(hwnd, 0, 0x7f, LWA_ALPHA);
+
     g_notifyIcon = new NotifyIcon(hwnd, 1, LoadIcon(nullptr, IDI_APPLICATION), TEXT("Hello world!"));
+
+    // ShowWindow(hwnd, SW_SHOW);
 
     MSG msg { };
     while (GetMessage(&msg, nullptr, 0, 0)) {
@@ -61,10 +65,37 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
         if (wParam == g_notifyIcon->GetID()) {
             switch (lParam) {
             case WM_RBUTTONDOWN:
-                
+                POINT position;
+
+                GetCursorPos(&position);
+                HMENU hMenu = CreatePopupMenu();
+
+                TCHAR text[] = TEXT("Quit");
+
+                MENUITEMINFO info;
+                info.cbSize = sizeof(MENUITEMINFO);
+                info.fMask = MIIM_ID | MIIM_STRING;
+                info.wID = 0;
+                info.dwTypeData = text;
+                info.cch = lstrlen(text);
+
+                InsertMenuItem(hMenu, 0, true, &info);
+
+                POINT point;
+                GetCursorPos(&point);
+                TrackPopupMenuEx(hMenu, TPM_LEFTALIGN, point.x, point.y, hwnd, NULL);
+                DestroyMenu(hMenu);
+
                 break;
             }
         }
+        return 0;
+
+    case WM_PAINT:
+        LPCWSTR text = TEXT("Hello world!");
+        HDC hdc = GetWindowDC(nullptr);
+        TextOut(hdc, 10, 10, text, lstrlen(text));
+        ReleaseDC(nullptr, hdc);
         return 0;
     }
 
