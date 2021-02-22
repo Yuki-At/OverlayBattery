e70a4b911f8b0548f6179fcbc35491fe1841d5c6
Yuki-At <yuki.yuki.yoh@gmail.com>
2021-02-22 21:24:04 +0900

    temp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,34 @@
 #include <Windows.h>
+#include <windowsx.h>
 #include "NotifyIcon.h"
 #include "Resource.h"
+#include "wintoastlib.h"
+
+
+class WinToastHandler : public WinToastLib::IWinToastHandler {
+public:
+    void toastActivated() const override { }
+    void toastActivated(int actionIndex) const override { }
+    void toastDismissed(WinToastDismissalReason state) const override { }
+    void toastFailed() const override { }
+};
+
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+bool OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+void OnDestroy(HWND hwnd);
+void OnTimer(HWND hwnd, UINT id);
+
 
 constexpr UINT IDM_QUIT = 1000;
 constexpr UINT IDM_CHANGEICON = 1001;
 
+
 HINSTANCE g_hInstance;
 NotifyIcon *g_notifyIcon;
+SYSTEM_POWER_STATUS g_prevPowerStatus;
+
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
     constexpr LPCTSTR WindowClassName = TEXT("MainWindowClass");
@@ -46,27 +66,20 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
         return -1;
     }
 
-    g_notifyIcon = new NotifyIcon(hwnd, 1, LoadIcon(nullptr, IDI_APPLICATION), TEXT("Hello world!"));
-
-    SetTimer(hwnd, 0, 10, nullptr);
-    SetLayeredWindowAttributes(hwnd, 0, 0x7f, LWA_ALPHA);
-    ShowWindow(hwnd, SW_SHOW);
-
     MSG msg { };
     while (GetMessage(&msg, nullptr, 0, 0)) {
         DispatchMessage(&msg);
     }
 
-    delete g_notifyIcon;
-
-    return msg.wParam;
+    return (int) msg.wParam;
 }
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+        HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+        HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+        HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
+        HANDLE_MSG(hwnd, WM_TIMER, OnTimer);
 
     case NotifyIcon::WM_NOTIFYICON:
         if (wParam == g_notifyIcon->GetID()) {
@@ -99,22 +112,67 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
             }
         }
         return 0;
-
-    case WM_TIMER:
-        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-        return 0;
-
-    case WM_COMMAND:
-        switch (LOWORD(wParam)) {
-        case IDM_QUIT:
-            SendMessage(hwnd, WM_CLOSE, 0, 0);
-            break;
-        case IDM_CHANGEICON:
-            g_notifyIcon->ChangeIcon(LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_OVERLAYBATTERY_ICON)));
-            break;
-        }
-        return 0;
     }
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+bool OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
+    g_notifyIcon = new NotifyIcon(hwnd, 1, LoadIcon(nullptr, IDI_APPLICATION), TEXT("OverlayBattery\n20%"));
+
+    SetTimer(hwnd, 0, 10, nullptr);
+    SetLayeredWindowAttributes(hwnd, 0, 0x7f, LWA_ALPHA);
+    ShowWindow(hwnd, SW_SHOW);
+
+    return true;
+}
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
+    switch (id) {
+    case IDM_QUIT:
+        SendMessage(hwnd, WM_CLOSE, 0, 0);
+        break;
+    case IDM_CHANGEICON:
+        g_notifyIcon->ChangeIcon(LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_OVERLAYBATTERY_ICON)));
+        break;
+    }
+}
+
+void OnDestroy(HWND hwnd) {
+    PostQuitMessage(0);
+    delete g_notifyIcon;
+}
+
+void OnTimer(HWND hwnd, UINT id) {
+    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
+    SYSTEM_POWER_STATUS status;
+    GetSystemPowerStatus(&status);
+
+    switch (status.ACLineStatus) {
+    case 0:
+        // offline
+        break;
+    case 1:
+        // online
+        break;
+    case 0xff:
+        // unknown
+        break;
+    }
+
+    if (status.BatteryFlag == 0xff) {
+        // unknown status
+    }
+
+    if (status.BatteryFlag | 0x80) {
+        // no system battery
+    }
+
+    if (status.BatteryFlag | 0x08) {
+        // charging
+    }
+
+    status.BatteryLifePercent; // percentage, if unknown status set 255.
+    status.BatteryLifeTime; // life time(sec), if unknown or connected AC line set -1.
 }
